4b51fd721fb6ad4c3cadcb01b666fb13b70e48de
Maf'j Alvarez <1101918+blujay@users.noreply.github.com>
2021-10-16 21:54:13 +0100

    making propcorn machine

---
+++ Assets/_scenes/propcorn/PropCorn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropCorn : MonoBehaviour
+{
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "popper")
+        {
+            //Debug.Log("popper hit-" + "I am " + this.name);
+            Transform prop = transform.GetChild(0);
+            prop.parent = null;
+            prop.gameObject.SetActive(true);
+            Destroy(transform.gameObject);
+        }
+    }
+}
---
+++ Assets/_scenes/propcorn/PropCornMaker.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PropCornMaker : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform cornSpawnPoint;
+    public GameObject cornPrefab;
+    public GameObject[] propPrefabs;
+    public int totalCorns;
+
+
+    void Start()
+    {
+        MakeCorn();
+    }
+
+    public void MakeCorn()
+    {
+        GameObject propCorns = new GameObject();
+        propCorns.name = "propCorns";
+        for (int i = 0; i < totalCorns; i++)
+        {
+            
+            GameObject cornKernel = Instantiate(cornPrefab, cornSpawnPoint.transform.position, transform.rotation, propCorns.transform);
+            cornKernel.name = "propCorn" + i;
+            GameObject newProp = PhotonNetwork.Instantiate(propPrefabs[Random.Range(0, propPrefabs.Length)].name, cornSpawnPoint.position, transform.rotation, 0);
+            //GameObject newProp = Instantiate(propPrefabs[Random.Range(0, propPrefabs.Length)], cornKernel.transform.position, transform.rotation);
+            newProp.transform.parent = cornKernel.transform;
+            cornKernel.AddComponent<PropCorn>();
+            HideProp(newProp);
+            
+        }
+    }
+
+    
+    public void HideProp(GameObject prop)
+    {
+        prop.gameObject.SetActive(false);
+    }
+}
+
+//todo
+// instantiate photon pun version of corns and prefabs from resources folder. 
+// 
+
+
+
+
+
